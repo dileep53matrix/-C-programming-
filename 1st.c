a52ec38ea39e5293939b8e8ebee8817eefc1e963
Dileep Choudhary <dileepchoudhary1008@gmail.com>
2025-10-15 19:19:21 +0530

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,72 @@
+// #include <stdio.h>
+
+// int main (){
+//     printf("dileep\n");
+//     return 0;
+
+// }
+
+#include <stdio.h>
+
+// int main(){
+//     int age ;
+//     // printf("enter your age\n");
+//     // scanf("%d",&age);
+//     int number ;
+//     // printf("enter your number\n");
+//     // scanf("%d", &number);
+
+
+//     return 0 ;
+
+// }
+
+// #include <stdio.h>
+
+// int main (){
+//     int age =19;
+//     printf("my age is %d\n", age);
+//     float pi = 3.14;
+//     printf("value of pi is %f\n", pi);
+//     char a = 'd';
+//     printf("Char is %c\n",a);
+//     return 0 ;
+// }
+
+// #include <stdio.h>
+
+// int main (){
+//     int a,b;
+//     printf("enter a\n");
+//     scanf("%d",&a);
+
+//     printf("enter b \n");
+//     scanf("%d", &b);
+
+//     printf("sum is :%d\n",a+b);
+//     return 0;
+
+// }
+
+// # include <stdio.h>
+// // area of square
+// int main(){
+//     int side ;
+//     // printf("side is %d \n");
+//     scanf("%d",&side);
+
+//     printf("area is %d\n", side *side );
+//     return 0;
+
+// }
+
+#include <stdio.h>
+// area of circle
+int main(){
+    int r ;
+    scanf("%d",&r);
+
+    printf("area is %f\n", 3.14 * r * r );
+    return 0;
+
+}
